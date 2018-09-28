84d4bdf07c25b153bd035c31c7fab20d3c8f2780
Allen Gleyzer <allen.gleyzer@ibm.com>
2018-09-28 14:44:16 -0400

    Always use manually installed jdk on linux

--- findJavaHome.js
+++ findJavaHome.js
@@ -1,7 +1,14 @@
-require('find-java-home')(function(err, home){
-  if(err){
-    console.error("[node-java] "+err);
-    process.exit(1);
-  }
-  process.stdout.write(home);
-});
+const os = require('os');
+let platform = os.platform();
+if(platform == 'linux') {
+  const basePath = process.env.WORKSPACE || process.env.CWD_INIT || '/home/vcap/app';
+  process.stdout.write(basePath + '/jdk')
+} else {
+  require('find-java-home')(function(err, home){
+    if(err){
+      console.error("[node-java] "+err);
+      process.exit(1);
+    }
+    process.stdout.write(home);
+  });
+}
